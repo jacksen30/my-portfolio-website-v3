80793397f620f91349f3311c6250b17036963300
Jacksen Nillson <nillsonjacksen@gmail.com>
2024-06-30 19:10:14 +0800

    Refactor toggleTheme and its required functions, add skills-github logo to update dynamically based on theme

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,14 @@ const themeToggle = document.getElementById('theme-toggle');
 const themeToggleIcon = document.getElementById('theme-toggle-icon');
 const githubStatCard = document.getElementById('github-stat-card');
 
+const skillsGithubLogo = document.getElementById('skill-github-logo');
+
 // Initialize theme from localStorage or set to dark theme if not present
 let theme = localStorage.getItem('theme') || 'dark-theme';
 root.classList.add(theme);
-updateThemeIcon();
-updateGithubStatCardTheme();
+checkForElementsRequiringThemingUpdate();
 
-function updateThemeIcon() {
+function checkForElementsRequiringThemingUpdate () {
     if (theme === 'dark-theme') {
         themeToggleIcon.classList.remove('fa-sun');
         themeToggleIcon.classList.add('fa-moon');
@@ -17,27 +18,30 @@ function updateThemeIcon() {
         themeToggleIcon.classList.remove('fa-moon');
         themeToggleIcon.classList.add('fa-sun');
     }
-}
 
-
-function updateGithubStatCardTheme() {
     if (githubStatCard !== null) {
         theme === 'dark-theme' ?  githubStatCard.src="https://streak-stats.demolab.com?user=jacksen30&theme=dark&hide_border=true&border_radius=0&date_format=j%2Fn%5B%2FY%5D&card_width=450&background=121212&sideNums=339900&currStreakNum=FF8D16&sideLabels=339900" : githubStatCard.src="https://streak-stats.demolab.com?user=jacksen30&theme=dark&hide_border=true&border_radius=0&date_format=j%2Fn%5B%2FY%5D&card_width=450&background=F7F9FC&sideNums=339900&currStreakNum=FF8D16&sideLabels=339900&dates=000000";
+    }
+
+    if (skillsGithubLogo !== null) {
+        theme === 'dark-theme' ?  skillsGithubLogo.src="./assets/images/github-darktheme-logo.png" : skillsGithubLogo.src="./assets/images/github-lighttheme-logo.png";
     }
 }
 
 function toggleTheme() {
     theme === 'dark-theme' ? theme = 'light-theme' : theme = 'dark-theme';
 
-    updateThemeIcon();
-    updateGithubStatCardTheme();
+    checkForElementsRequiringThemingUpdate();
 
     root.className = theme;
     localStorage.setItem('theme', theme);
 }
 
 
-themeToggle.addEventListener('click', () => {
-    toggleTheme();
-});
+themeToggle.addEventListener('click', toggleTheme);
+
+
+
+
+
 
